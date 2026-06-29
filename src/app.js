a877e82922f4a8eb66c87b418a170a0c0ce7d380
Hope Mutindi <msndindi@outlook.com>
2026-06-29 13:11:43 +0200

    Inject Weather details

--- src/app.js
+++ src/app.js
@@ -1,12 +1,48 @@
+
 function refreshWeatherData(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  let temperature =temperatureElement.innerHTML;
-  
+  let temperature = temperatureElement.innerHTML;
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let date = new Date(response.data.time * 1000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
+  console.log(response.data);
 }
+
+function formatDate(date) {
+  
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes} ,`;
+}
+
+
+
 
 function searchCity(city) {
   let apiKey = "00fd2534b306ca4ac44778fc7b36o08t";
@@ -18,12 +54,11 @@ function searchCity(city) {
 function handleSearchSubmission(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-form-input");
-  
+
   searchCity(searchInputElement.value);
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSearchSubmission);
-
 
 searchCity("Oslo"); // Default city when the page loads
