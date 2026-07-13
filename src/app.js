e8b2c61636b261b25af8b61b6abbf9a0193d7ee3
Hope Mutindi <msndindi@outlook.com>
2026-07-13 21:21:32 +0200

    Inject real weather forecast data

--- src/app.js
+++ src/app.js
@@ -84,22 +84,26 @@ function getForecast(city) {
 
 function displayForecast(response) {
   console.log(response.data);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  // let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  // loop through response.data and get the day of the week for each forecast day
 
   let forecastHtml = "";
 
-  days.forEach(function (day) {
+  response.data.daily.forEach(function (day,) {
     forecastHtml =
       forecastHtml +
       `
     <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date"></div>
+        <div class="weather-forecast-icon">
+        <img src="${day.condition.icon_url}"  />
+    
+        </div>
         <div class="weather-forecast-temperatures">
           <div class="weather-forecast-temperature">
-            <strong>15º</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
           </div>
-          <div class="weather-forecast-temperature">9º</div>
+          <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
         </div>
       </div>
       `;
@@ -110,4 +114,4 @@ function displayForecast(response) {
 }
 
 searchCity("Oslo"); // Default city when the page loads
-// getForecast("Oslo"); // Fetch forecast for the default city
+getForecast("Oslo"); // Fetch forecast for the default city
